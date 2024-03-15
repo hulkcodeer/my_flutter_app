81eb38496e7700411834ed5a98ccd7173a2b2f16
Hyun Jin Park <>
2024-03-15 18:38:53 +0900

    :construction: 홈화면 따라 만들기 UI 추가

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,88 @@ void main() {
 // 그중에 StatelessWidget부터 상속받아서 해본다.
 // App Root에 Return해야 하는 앱이 두개 있는데, 하나는 material, 다른 하나는 cupertino 앱을 리턴하는것
 // scaffold는 무조건 써야한다.
-class App extends StatelessWidget{
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Wow'),
-          centerTitle: false,          
-          titleTextStyle: TextStyle(color: Colors.purple),
-          elevation: 99,
-        ),
-        body: Center(
-          child: Text('Hellow world!'),
-        ) ,
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Hyunjin',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$5 192 482',
+                  style: TextStyle(
+                      fontSize: 48,
+                      color: Colors.white.withOpacity(0.8),
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFFF2B33A),
+                          borderRadius: BorderRadius.circular(45)),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
